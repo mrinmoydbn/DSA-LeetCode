3962fc40890da746baae6b226d17f25f8f8ddb5e
Mrinmoy Debnath <160152141+mrinmoydbn@users.noreply.github.com>
2026-09-19 13:14:44 +0530

    Time: 4 ms (34.20%) | Memory: 263.4 MB (38.55%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    void reverse (vector<int>& nums, int left, int right) {
+        while (left < right) {
+            swap(nums[left], nums[right]);
+            left++;
+            right--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+
+        int n = nums.size();
+        
+        if (n == 0 || k == 0) {
+            return;
+        }
+        k = k % n;
+        if (k == 0) {
+            return;
+        }
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+};
